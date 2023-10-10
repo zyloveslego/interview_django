8265cbfab4ecca47f15fed8a7ec9284e11d89680
zyloveslego <husyzy79@gmail.com>
2023-10-10 19:11:48 -0400

    some print

--- interview_django_v2/settings.py
+++ interview_django_v2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^c4x*w5vu9bx=1)1yt3cw#h4p*w@uy5cri2r-z-r+636m6)0=-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
