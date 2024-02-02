58ded893d19735f31e45945ba82fcbaf9a561cef
zenniezzz <131829621+zenniezzz@users.noreply.github.com>
2024-02-01 22:07:51 -0500

    setup page new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,23 @@ import Setup from './pages/interviewsetup';
 import { getQuestions } from './setup';
 import { useTheme, styled, Typography, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, MenuItem, InputLabel, Select, Paper } from '@mui/material/';
 import InterviewQuestion from './pages/interviewquestion';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<InterviewQuestion />}>
+    <Route path="interviewsetup" element={<Setup />} />
+    <Route path="interviewquestion" element={<InterviewQuestion />} />
+  </Route>
+));
 
 function App() {
 
-
-
   return (
-    <div>
-      <InterviewQuestion />
-    </div>
+    <RouterProvider router={router} />
+    // <div>
+    //   <Nav />
+
+    // </div>
 
   );
 }
--- frontend/src/js/timer.js
+++ frontend/src/js/timer.js
@@ -15,15 +15,15 @@ stopButton.addEventListener('click', () => {
     clearInterval(interval);
     timerElement.textContent = '00:00';
     startTime = 0;
-});
+  });
 
 function updateTimer() {
-    const currentTime = Date.now() - startTime;
-    const minutes = Math.floor(currentTime / 60000);
-    const seconds = Math.floor((currentTime % 60000) / 1000);
-
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(seconds).padStart(2, '0');
-
-    timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
+  const currentTime = Date.now() - startTime;
+  const minutes = Math.floor(currentTime / 60000);
+  const seconds = Math.floor((currentTime % 60000) / 1000);
+  
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+  
+  timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
 }
